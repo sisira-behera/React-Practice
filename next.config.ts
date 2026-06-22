1e8fb07197cce9d8d17eb8be74be4ed989351e90
Sisira Behera <sisira.behera@knacksystems.com>
2026-06-22 14:17:18 +0530

    Updated for the github deployment

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,9 @@ const nextConfig: NextConfig = {
   experimental: {
     globalNotFound: true,
   },
+  output: 'export', // Forces Next.js to build static HTML files
   images: {
+    unoptimized: true, // Required because GitHub Pages doesn't support image optimization API
     remotePatterns: [
       {
         protocol: 'https',
